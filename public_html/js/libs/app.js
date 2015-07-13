d7a2e95e36e9a58a92f97b112b0bf7706a61d246
Psymatix <psymatix@gmail.com>
2015-07-13 17:30:25 +0100

    Working with global hjs and deps

--- public_html/js/libs/app.js
+++ public_html/js/libs/app.js
@@ -1,42 +1,43 @@
-define(["jquery-1.9.0/jquery.min"], function(){
-    
+define(["jquery", "greeting"], function($){
+
+        var App = App || {};
+
+        App.init = function(){
+            console.log("here");
+          $('[data-action]').each(function(){
+              $(this).on("click", function(event){
+                  var $this = $(this),
+                  data = $(this).data();
+
+                  App.actions[$(this).data("action")]($this, data);
+
+                  event.preventDefault();
+              });
+          });
+
+        };
+
+        App.actions = {
+
+          showSection: function($this, data){
+              var greetingTemplate = templates["greeting"],
+                      listTemplate = templates["todolist"];
+
+              var templatedata = {"name":"Simon"};
+              var listData = {
+                "list":[
+                    {"item":"bread"},
+                    {"item":"milk"},
+                    {"item":"sugar"}
+                ]  
+              };
+
+              $("#showArea").html( greetingTemplate.render(templatedata) );
+               $("#showArea").after("<div>" + listTemplate.render(listData) + "</div>");
+          }
+
+        };
+
+return App;
+
 });
-
-var App = App || {};
-
-App.init = function(){
-
-  $('[data-action]').each(function(){
-      $(this).on("click", function(event){
-          var $this = $(this),
-          data = $(this).data();
-          
-          App.actions[$(this).data("action")]($this, data);
-          
-          event.preventDefault();
-      });
-  });
-    
-};
-
-App.actions = {
-  
-  showSection: function($this, data){
-      var greetingTemplate = templates["greeting"],
-              listTemplate = templates["todolist"];
-      
-      var templatedata = {"name":"Simon"};
-      var listData = {
-        "list":[
-            {"item":"bread"},
-            {"item":"milk"},
-            {"item":"sugar"}
-        ]  
-      };
-      
-      $("#showArea").html( greetingTemplate.render(templatedata) );
-       $("#showArea").after("<div>" + listTemplate.render(listData) + "</div>");
-  }
-  
-};
-
--- public_html/js/libs/main.js
+++ public_html/js/libs/main.js
@@ -1,12 +1,15 @@
 requirejs.config({
-    
+    paths: {
+       
+        'jquery': 'jquery-1.9.0/jquery.min',
+        'greeting':'../../compiled_templates/greeting'
+        
+    }
 });
 
 
-//app.init
 
-require(["app"], function(app){
+requirejs(['app'], function(app){
     app.init();
 });
-
 
